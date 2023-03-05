cf7661bfd839e6f2dda27957d01b9b205b4e151b
mkparmar1 <mkparmar.131@gmail.com>
2023-03-06 00:36:19 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { TextForm } from './components/TextForm/TextForm';
+
+function App() {
+  return ( 
+    <>
+    <Navbar title="TextUtils" aboutText="About" />
+    <TextForm/>    
+    </>    
+  );
+}
+
+export default App;
---
+++ src/components/TextForm/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TextForm = () => {
+  return (
+    <>
+        <div class="mb-3">
+            <label for="myBox" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="myBox" rows="3"></textarea>
+        </div>
+    </>
+  )
+}
