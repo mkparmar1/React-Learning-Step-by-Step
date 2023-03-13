ec67ed204bc108210b0210d5724e69aa32d44ff7
mkparmar1 <mkparmar.131@gmail.com>
2023-03-13 23:39:54 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import { TextForm } from './components/TextForm/TextForm';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { TextForm } from "./components/TextForm/TextForm";
 
 function App() {
-  return ( 
+  return (
     <>
-    <Navbar title="TextUtils" aboutText="About" />
-    <TextForm/>    
-    </>    
+      <Navbar title="TextUtils" aboutText="About" />
+      <TextForm />
+    </>
   );
 }
 
